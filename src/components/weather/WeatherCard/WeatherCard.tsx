e4b7ec51e06c27a0d178073e0c1fee1817188382
obemeuche <obemeuchechi@gmail.com>
2026-04-02 15:33:52 +0100

    remove location icon

--- src/components/weather/WeatherCard/WeatherCard.tsx
+++ src/components/weather/WeatherCard/WeatherCard.tsx
@@ -37,9 +37,6 @@ export function WeatherCard({ data, displayCity, displayCountry }: WeatherCardPr
       </div>
 
       <div className={styles.locationBlock}>
-        <svg className={styles.locationPin} width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-        </svg>
         <span className={styles.cityName}>{displayCity}</span>
         {displayCountry && <span className={styles.countryName}>{displayCountry}</span>}
       </div>
